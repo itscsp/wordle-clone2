b5ebb59a7cf84e49633b7e185e7818ca9fc4cb26
itscsp <cspoojary7972@gmail.com>
2022-03-05 07:42:29 +0530

    bug fixes

--- script.js
+++ script.js
@@ -1,2 +1,247 @@
-"use strict";var pressedLetter,letters,lettersPettern=/[A-Za-z]/,animationCompleted=!1,isTyping=!0,currentGuessCount=1,currentGuess=document.querySelector("#guess"+currentGuessCount),wordList=null,solutionWord=void 0,completeWordlist=void 0,updateLetters=function(e){letters=currentGuess.dataset.letters;var t=(currentGuess.dataset.letters=letters+e).length;updateTail(t,e)};document.addEventListener("keyup",(function(e){var t=e.key,s=lettersPettern.test(t);if(1===t.length&&s&&currentGuess.dataset.letters.length<=4&&isTyping)console.log("is letter"),updateLetters(t);else if("Backspace"==e.key&&""!=currentGuess.dataset.letters)deleteFromLetters();else if(5==currentGuess.dataset.letters.length&&"Enter"===e.key){if(validWord())for(var r=function(e){setTimeout((function(){revealTile(e,checkLetter(e))}),500*e)},n=0;n<6;n++)r(n)}}));var checkWin=function(){return solutionWord==currentGuess.dataset.letters},updateTail=function(e,t){var s=document.querySelector("#guess"+currentGuessCount+"Tile"+e);s.innerText=t,s.classList.add("has-letter")},deleteFromLetters=function(){var e=currentGuess.dataset.letters,t=e.slice(0,-1);currentGuess.dataset.letters=t,deleteFromTiles(e.length)},deleteFromTiles=function(e){var t=document.querySelector("#guess"+currentGuessCount+"Tile"+e);t.innerText="",t.classList.remove("has-letter")},checkLetter=function(e){if(e<5){var t=currentGuess.dataset.letters.charAt(e);return t===solutionWord.charAt(e)?"correct":solutionWord.includes(t)?"present":"absent"}},revealTile=function(e,t){e<5&&flipTile(e,t),console.log(e),5===e&&(checkWin()?(setTimeout((function(){alert("Brillent! \n You Win The Match")}),1500),jumpTiles()):(currentGuessCount+=1,currentGuess=document.querySelector("#guess"+currentGuessCount),isTyping=!0,currentGuessCount>6&&setTimeout((function(){showSolution()}),500)))},showSolution=function(){alert("You Last the game \n Solution Word : "+solutionWord)},flipTile=function(e,t){var s=document.querySelector("#guess"+currentGuessCount+"Tile"+(e+1));s.classList.add("flip-in"),setTimeout((function(){s.classList.add(t),s.classList.remove("flip-in"),s.classList.add("flip-out")}),500),setInterval((function(){s.classList.remove("flip-out")}),1e3)},jumpTiles=function(){for(var e=function(e){var t=document.querySelector("#guess"+currentGuessCount+"Tile"+(e+1));setTimeout((function(){t.classList.add("jump")}),250*e)},t=0;t<5;t++)e(t)};function getRandomNumber(e){var t=new XMLHttpRequest;t.open("GET",e,!1),t.onreadystatechange=function(){if(4===t.readyState&&(200===t.status||0==t.status)){wordList=t.responseText.split("\r\n");var e=Math.floor(Math.random()*wordList.length-1);completeWordlist=t.responseText,solutionWord=wordList[e]}},t.send(null)}function validWord(){return!!completeWordlist.includes(currentGuess.dataset.letters)||(alert("Not in Word List"),!1)}getRandomNumber("./words.txt");
-//# sourceMappingURL=script.js.map
+/*
+- if keypress is a letter
+- update "letters" attribute
+  - update tile markup based on "letters" value
+
+- if keypress is backspace key
+  - delete last letter in "letters"
+	  - update tile markup based on "letters"
+
+*/
+
+const lettersPettern = /[A-Za-z]/;
+let pressedLetter;
+let  animationCompleted = false;
+let isTyping = true;
+
+let currentGuessCount = 1;
+let currentGuess = document.querySelector("#guess" + currentGuessCount);
+
+let wordList = null; // containing all word in array format
+let solutionWord = undefined; //getting random word from getRandomNumber()
+let completeWordlist = undefined; // containing all words
+
+// update "letters"
+let letters;
+const updateLetters = (letter) => {
+
+  letters = currentGuess.dataset.letters;
+  let tileNumber = (currentGuess.dataset.letters = letters + letter).length;
+  updateTail(tileNumber, letter);
+};
+
+// letters typing events (lette)
+document.addEventListener("keyup", (e) => {
+  let keypress = e.key;
+  let isLetter = lettersPettern.test(keypress);
+
+  if (
+    keypress.length === 1 &&
+    isLetter &&
+    currentGuess.dataset.letters.length <= 4 && isTyping
+  ) {
+    console.log('is letter')
+    updateLetters(keypress);
+  } else if (e.key == "Backspace" && currentGuess.dataset.letters != "") {
+    deleteFromLetters();
+  } else if (currentGuess.dataset.letters.length == 5 && e.key === "Enter") {
+    let wordvalid = validWord();
+
+    if (wordvalid) {
+      //if word is valid then loop run
+      for (let i = 0; i < 6; i++) {
+        setTimeout(() => {
+          revealTile(i, checkLetter(i));
+        }, i*500);
+      }
+
+
+
+    }
+  }
+});
+
+
+const checkWin = () => {
+  return solutionWord == currentGuess.dataset.letters ? true : false;
+};
+
+
+// updating tail
+
+const updateTail = (tileNumber, letter) => {
+  let currentTail = document.querySelector("#guess"+currentGuessCount+"Tile" + tileNumber);
+  // currentTail.dataset.letter = letter
+  currentTail.innerText = letter;
+  currentTail.classList.add('has-letter');
+};
+
+//Backspace -- delete last letters
+
+const deleteFromLetters = () => {
+  // remove last letter from data.letters
+  let oldLetters = currentGuess.dataset.letters;
+
+  let newLetters = oldLetters.slice(0, -1);
+  currentGuess.dataset.letters = newLetters;
+
+  deleteFromTiles(oldLetters.length);
+};
+
+// Backspace -- delete last tile markup
+const deleteFromTiles = (len) => {
+  //remove markup from last tile
+  let currentTail = document.querySelector("#guess"+currentGuessCount+"Tile" + len);
+  // currentTail.dataset.letter = letter
+  currentTail.innerText = "";
+
+  currentTail.classList.remove('has-letter'); // remove class if he press backspace
+};
+
+/* Check letter to solution
+ */
+const checkLetter = (positions) => {
+
+  if(positions < 5){
+
+    let guessedLetter = currentGuess.dataset.letters.charAt(positions);
+    let solutionLetter = solutionWord.charAt(positions);
+
+    // 1. check if letter is matching with solutionWord ("Correct")
+    if (guessedLetter === solutionLetter) {
+      return 'correct'
+    } else {
+      return solutionWord.includes(guessedLetter)
+        ?  'present'
+        :  'absent';
+    }
+  }
+
+};
+
+
+
+const revealTile = (i,status) => {
+  if(i < 5){
+    flipTile(i,status);
+  }
+
+  console.log(i)
+
+
+    if(i === 5){
+      if(checkWin()){
+        setTimeout(() => {
+          alert('Brillent! \n You Win The Match')
+        }, 1500)
+        jumpTiles();
+      }else{
+
+        currentGuessCount = currentGuessCount + 1;
+        currentGuess = document.querySelector("#guess" + currentGuessCount);
+        isTyping = true;
+
+        if(currentGuessCount > 6){
+          setTimeout(() => {
+            showSolution();
+          }, 500)
+        }
+      }
+    }
+
+
+}
+
+const showSolution = () => {
+    alert("You Last the game \n Solution Word : "+solutionWord)
+}
+
+
+
+
+// Flip in and out animation
+
+const flipTile = (i, status) => {
+  let currentElement = document.querySelector("#guess"+currentGuessCount+"Tile" + (i + 1));
+
+  currentElement.classList.add('flip-in')
+
+
+  setTimeout(() => {
+    currentElement.classList.add(status);
+    currentElement.classList.remove('flip-in');
+    currentElement.classList.add('flip-out');
+  }, 500)
+
+
+  setInterval(() => {
+    currentElement.classList.remove("flip-out")
+
+
+  }, 1000)
+
+
+}
+
+//jump animation if win
+const jumpTiles = () => {
+  for(let i=0; i < 5; i ++){
+    let currentTile = document.querySelector('#guess'+currentGuessCount+"Tile" + (i + 1))
+    setTimeout(() => {
+      currentTile.classList.add('jump');
+    }, i*250)
+  }
+}
+
+
+/*************
+Generate random Solution Word
+************* */
+
+function getRandomNumber(file) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        wordList = rawFile.responseText.split("\r\n");
+        let randomIndex = Math.floor(Math.random() * wordList.length - 1);
+
+        completeWordlist = rawFile.responseText;
+
+        solutionWord = wordList[randomIndex];
+      }
+    }
+  };
+  rawFile.send(null);
+}
+
+getRandomNumber("./words.txt");
+
+// check if user inputed word is exists in our list
+function validWord() {
+  if (completeWordlist.includes(currentGuess.dataset.letters)) {
+    return true;
+  } else {
+    alert("Not in Word List");
+    return false;
+  }
+}
+
+
+
+
+
+  // switch(status){
+  //   case 'correct':
+  //     currentElement.classList.add("correct");
+  //     break;
+  //   case 'present':
+  //     currentElement.classList.add("present")
+  //     break;
+  //   case 'absent':
+  //     currentElement.classList.add("absent");
+  //     break;
+  //   default:
+  //     console.error("Some thing is wrong")
+  // }
